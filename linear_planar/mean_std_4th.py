0ed13b25c12f30d4305381c23478c69784cdda33
Daniel Rose <drose@cbs.mpg.de>
2020-11-10 13:56:21 +0100

    refactored command line script into command line tool with function

--- linear_planar/mean_std_4th.py
+++ linear_planar/mean_std_4th.py
@@ -2,11 +2,10 @@ import nibabel as nib
 import numpy as np
 import sys
 
-if __name__ == "__main__":
-    print('\nparam order::\ninput_vol.nii(.gz) output_vol_mean.nii(.gz) output_vol_std.nii(.gz)\n')
 
+def mean_std_from_timeseries(in_file: str, out_mean_file: str, out_std_file: str):
     # loading data
-    img = nib.load(sys.argv[1])
+    img = nib.load(in_file)
     data = img.get_data()
 
     newdata1 = data.mean(axis=3)
@@ -15,7 +14,13 @@ if __name__ == "__main__":
     newimg1 = nib.nifti1.Nifti1Image(newdata1, img.affine, img.header)
     newimg2 = nib.nifti1.Nifti1Image(newdata2, img.affine, img.header)
 
-    nib.save(newimg1, sys.argv[2])
-    nib.save(newimg2, sys.argv[3])
+    nib.save(newimg1, out_mean_file)
+    nib.save(newimg2, out_std_file)
+
+    return out_mean_file, out_std_file
 
 
+if __name__ == "__main__":
+    print('\nparam order::\ninput_vol.nii(.gz) output_vol_mean.nii(.gz) output_vol_std.nii(.gz)\n')
+
+    mean_std_from_timeseries(*sys.argv)
